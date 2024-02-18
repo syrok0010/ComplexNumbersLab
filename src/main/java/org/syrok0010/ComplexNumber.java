8c45f86f358123e3c1d9e1150959ae5aa4c3291e
syrok0010 <steamruler@mail.ru>
2024-02-18 21:45:24 +0300

    Rename Complex number methods to follow Java conventions and add to string method

--- src/main/java/org/syrok0010/ComplexNumber.java
+++ src/main/java/org/syrok0010/ComplexNumber.java
@@ -4,6 +4,11 @@ public class ComplexNumber {
     private final double Re;
     private final double Im;
 
+    public ComplexNumber(double re) {
+        Re = re;
+        Im = 0;
+    }
+
     public ComplexNumber(double re, double im) {
         Re = re;
         Im = im;
@@ -17,37 +22,37 @@ public class ComplexNumber {
         return Re;
     }
 
-    public ComplexNumber Add(ComplexNumber other) {
+    public ComplexNumber add(ComplexNumber other) {
         if (other == null)
             return this;
         return new ComplexNumber(this.Re + other.getRe(), this.Im + other.getIm());
     }
 
-    public ComplexNumber Add(double other) {
+    public ComplexNumber add(double other) {
         return new ComplexNumber(this.Re + other, this.Im);
     }
 
-    public ComplexNumber Subtract(ComplexNumber other) {
+    public ComplexNumber subtract(ComplexNumber other) {
         if (other == null)
             return this;
         return new ComplexNumber(this.Re - other.getRe(), this.Im - other.getIm());
     }
 
-    public ComplexNumber Subtract(double other) {
+    public ComplexNumber subtract(double other) {
         return new ComplexNumber(this.Re - other, this.Im);
     }
 
-    public ComplexNumber Multiply(ComplexNumber other) {
+    public ComplexNumber multiply(ComplexNumber other) {
         if (other == null)
             return this;
         return new ComplexNumber(this.Re * other.Re - this.Im * other.Im, this.Im * other.Re + this.Re * other.Im);
     }
 
-    public ComplexNumber Multiply(double other) {
-        return Multiply(new ComplexNumber(other, 0));
+    public ComplexNumber multiply(double other) {
+        return multiply(new ComplexNumber(other, 0));
     }
 
-    public ComplexNumber Divide(ComplexNumber other) {
+    public ComplexNumber divide(ComplexNumber other) {
         if (other == null)
             return this;
         var a = this.Re;
@@ -59,7 +64,13 @@ public class ComplexNumber {
         return new ComplexNumber(re, im);
     }
 
-    public ComplexNumber Divide(double other) {
-        return Divide(new ComplexNumber(other, 0));
+    public ComplexNumber divide(double other) {
+        return divide(new ComplexNumber(other, 0));
+    }
+
+    @Override
+    public String toString() {
+        if (Im == 0) return Double.toString(Re);
+        return Re + " + " + Im + "i";
     }
 }
